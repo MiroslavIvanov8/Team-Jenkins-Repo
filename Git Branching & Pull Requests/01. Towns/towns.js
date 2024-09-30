5cdab78b6dd561dd36cc71651da2797580229602
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-09-30 13:12:08 +0300

    Improved styling and effects for the functionality to add a new town

--- Git Branching & Pull Requests/01. Towns/towns.js
+++ Git Branching & Pull Requests/01. Towns/towns.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     $('#btnAdd').click(addTown);
 });
 
+$(document).ready(function () {    
+    $('#btnDelete').click(deleteTown);
+});
+
 function addTown() {
     let townName = $('#townNameForAdd').val().trim();
     $('#townNameForAdd').val('');  
@@ -15,3 +19,26 @@ function addTown() {
         $('#result').show(); 
     }
 }
+function deleteTown() {
+    let townName = $('#townName').val();
+    $('#townName').val('');
+    let removed = false;
+    for (let option of $('#towns option')) {
+        if (option.textContent == townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+    if (removed)
+        showMessage(townName + " deleted.");
+    else
+        showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+    $('#result').text(msg).css("display", "block");
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
+}
+
